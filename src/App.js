fa7bae484f97cd806012d91114b903868a57c34e
kangana <kangana512@gmail.com>
2017-04-19 15:36:52 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import { Scene, Router } from 'react-native-router-flux';
+import Login from './components/Login'
+
+export default class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Scene key="root">
+                    <Scene key="login" component={Login} title="Login" />
+                </Scene>
+            </Router>
+        );
+    }
+}
