e08f69fa05fe6159fcf9b218d84bc4c3a0fc247f
kangana <kangana512@gmail.com>
2017-05-03 15:05:47 +0700

    Link

--- src/components/ContentFeed.js
+++ src/components/ContentFeed.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ListView, Image } from 'react-native';
+import { View, Text, ListView, Image, TouchableOpacity, Linking } from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Card, Thumbnail, CardItem } from 'native-base';
 
 const apiKey = 'AIzaSyAFYk7Kahr_8n-mTCE29K-x5lv2kgrd1aA';
@@ -30,8 +30,9 @@ class ContentFeed extends Component {
     }
     renderRowCard(rowData) {
         /* Map Data */
+        let {videoId} = rowData.id;
         let { title, description, publishedAt, thumbnails } = rowData.snippet;
-        let {high} = thumbnails;
+        let { high } = thumbnails;
         /* Map Data */
         return (
             <Card >
@@ -39,17 +40,26 @@ class ContentFeed extends Component {
                     <Left>
                         <Thumbnail source={{ uri: 'https://yt3.ggpht.com/-3rayVyHvRWI/AAAAAAAAAAI/AAAAAAAAAAA/B14_OGQpBvA/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' }} />
                         <Body>
-                            <Text>{title}</Text>
+                            <Text style={{
+                                fontWeight: 'bold',
+                                fontSize: 16
+                            }}>{title}</Text>
                             <Text note>{publishedAt}</Text>
                         </Body>
                     </Left>
                 </CardItem>
                 <CardItem cardBody>
                     <Body>
-                        <Image source={{uri:high.url}} style={{
-                            height:200,
+                        <TouchableOpacity style={{
                             alignSelf:'stretch'
-                        }}/>
+                        }}
+                        onPress={()=>Linking.openURL('https://www.youtube.com/watch?v='+videoId)}
+                        >
+                            <Image source={{ uri: high.url }} style={{
+                                height: 200,
+                                alignSelf: 'stretch'
+                            }} />
+                        </TouchableOpacity>
                     </Body>
                 </CardItem>
                 <CardItem content>
