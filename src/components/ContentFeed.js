15f42029305a75df73cd88348c6ffe14abb45d64
kangana <kangana512@gmail.com>
2017-05-03 14:46:35 +0700

    feed fix

--- src/components/ContentFeed.js
+++ src/components/ContentFeed.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, ListView ,Image} from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon,Card,Thumbnail,CardItem } from 'native-base';
+import { View, Text, ListView, Image } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Card, Thumbnail, CardItem } from 'native-base';
 
 const apiKey = 'AIzaSyAFYk7Kahr_8n-mTCE29K-x5lv2kgrd1aA';
 const channelID = 'UC5mJrJ6tg1WiGGWzoKK7ewQ';
@@ -11,9 +11,9 @@ class ContentFeed extends Component {
     }
     async getJsonFeed() {
         try {
-            const response = await fetch('https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&channelId=' + channelID + '&part=snippet,id&order=date&maxResults=20');
+            const response = await fetch('https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&channelId=' + channelID + '&part=snippet,id&order=date&maxResults=20');// ข้อมูลดิบ
 
-            const responseJson = await response.json();
+            const responseJson = await response.json(); // แปลงเป็น Json
 
             this.setState({ dataSource: this.state.dataSource.cloneWithRows(responseJson.items) });
 
@@ -29,7 +29,10 @@ class ContentFeed extends Component {
         };
     }
     renderRowCard(rowData) {
+        /* Map Data */
         let { title, description, publishedAt, thumbnails } = rowData.snippet;
+        let {high} = thumbnails;
+        /* Map Data */
         return (
             <Card >
                 <CardItem>
@@ -37,17 +40,20 @@ class ContentFeed extends Component {
                         <Thumbnail source={{ uri: 'https://yt3.ggpht.com/-3rayVyHvRWI/AAAAAAAAAAI/AAAAAAAAAAA/B14_OGQpBvA/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' }} />
                         <Body>
                             <Text>{title}</Text>
-                            <Text note>GeekyAnts</Text>
+                            <Text note>{publishedAt}</Text>
                         </Body>
                     </Left>
                 </CardItem>
                 <CardItem cardBody>
-                    <Image />
+                    <Body>
+                        <Image source={{uri:high.url}} style={{
+                            height:200,
+                            alignSelf:'stretch'
+                        }}/>
+                    </Body>
                 </CardItem>
                 <CardItem content>
-                    <Text>Wait a minute. Wait a minute, Doc. Uhhh...
-                              Are you telling me that you built a time machine... out of a DeLorean?!
-                              Whoa. This is heavy.</Text>
+                    <Text>{description}</Text>
                 </CardItem>
             </Card>
         )
